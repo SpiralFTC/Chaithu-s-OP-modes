4fcc588f753dd33567b4bf9ff585e0d594f24ffb
Chaithu1 <chaithudikkala@gmail.com>
2015-11-10 19:29:21 -0800

    Merge branch 'master' of https://github.com/SpiralFTC/Chaithu-s-OP-modes into mtroll
    # Conflicts:
    #	ftc_app-master/ftc_app-master/FtcRobotController/src/main/java/com/qualcomm/ftcrobotcontroller/opmodes/TankDrive.java

--- ftc_app-master/ftc_app-master/FtcRobotController/src/main/java/com/qualcomm/ftcrobotcontroller/opmodes/TankDrive.java
+++ ftc_app-master/ftc_app-master/FtcRobotController/src/main/java/com/qualcomm/ftcrobotcontroller/opmodes/TankDrive.java
@@ -2,6 +2,7 @@ package com.qualcomm.ftcrobotcontroller.opmodes;
 
 import com.qualcomm.robotcore.eventloop.opmode.OpMode;
 import com.qualcomm.robotcore.hardware.DcMotor;
+import com.qualcomm.robotcore.hardware.Servo;
 import com.qualcomm.robotcore.util.Range;
 
 
@@ -10,17 +11,26 @@ import com.qualcomm.robotcore.util.Range;
  */
 public class TankDrive extends OpMode {
 
-    DcMotor urmom;
-    DcMotor yo;
+
+    DcMotor right;
+    DcMotor left;
+    Servo RightServo;
+    Servo LeftServo;
+    double ServoPosition;
 
     public TankDrive() {
     }
 
     @Override
     public void init() {
-        urmom = hardwareMap.dcMotor.get("right");//right forward
-        yo = hardwareMap.dcMotor.get("left");
-        yo.setDirection(DcMotor.Direction.REVERSE);
+        right = hardwareMap.dcMotor.get("right");//right forward
+        left = hardwareMap.dcMotor.get("left");
+        left.setDirection(DcMotor.Direction.REVERSE);
+        RightServo = hardwareMap.servo.get("rightS");
+        LeftServo = hardwareMap.servo.get("leftS");
+        ServoPosition = .5;
+        RightServo.setPosition(ServoPosition);
+        LeftServo.setPosition(ServoPosition);
     }
 
     @Override
@@ -30,25 +40,25 @@ public class TankDrive extends OpMode {
 
     @Override
     public void loop() {
+        RightServo.setPosition(ServoPosition);
+        LeftServo.setPosition(ServoPosition);
         double rightPower = -gamepad1.left_stick_y;
-        double leftPower = -gamepad1.right_stick_x;
-
+        double leftPower = -gamepad1.right_stick_y;
         rightPower = Range.clip(rightPower, -1, 1);
         leftPower = Range.clip(leftPower, -1, 1);
+        right.setPower(-leftPower);
+        left.setPower(rightPower);
+        if (gamepad1.a) {
+            double ServoPosition = this.ServoPosition - .1;
+            RightServo.setPosition(ServoPosition);
+            LeftServo.setPosition(ServoPosition);
 
-        if (gamepad1.right_stick_x > 0) {
-            urmom.setPower(leftPower);
-            yo.setPower(-leftPower);
-        } else if (gamepad1.right_stick_x < 0){
-            urmom.setPower(-leftPower);
-            yo.setPower(leftPower);
-        } else {
-            urmom.setPower(0);
-            yo.setPower(0);
         }
-
-        urmom.setPower(rightPower);
-        yo.setPower(rightPower);
+        if (gamepad1.y) {
+            double ServoPosition = this.ServoPosition + .1;
+            RightServo.setPosition(ServoPosition);
+            LeftServo.setPosition(ServoPosition);
+        }
 
     }
 }
