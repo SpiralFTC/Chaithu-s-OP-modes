86c5c0bd86dc46f28556f3052ecc723fa0e6b5d4
Ritika13 <tikkajain@gmail.com>
2015-11-20 19:47:05 -0800

    Beacon

---
+++ ftc_app-master/FtcRobotController/src/main/java/com/qualcomm/ftcrobotcontroller/opmodes/Beacon.java
@@ -0,0 +1,42 @@
+package com.qualcomm.ftcrobotcontroller.opmodes;
+import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
+import com.qualcomm.robotcore.hardware.DcMotor;
+import com.qualcomm.robotcore.hardware.LightSensor;
+
+/**
+ * Created by ritika on 11/17/15.
+ */
+public class Beacon extends LinearOpMode {
+
+    DcMotor motorRight;
+    DcMotor motorLeft;
+    LightSensor LS;
+
+    @Override
+    public void runOpMode() throws InterruptedException {
+
+        motorRight = hardwareMap.dcMotor.get("motor_Right");
+        motorLeft = hardwareMap.dcMotor.get("motor_Left");
+        LS = hardwareMap.lightSensor.get("light_Sensor");
+
+        motorLeft.setDirection(DcMotor.Direction.REVERSE);
+
+        LS.enableLed(true);
+        double LightValue = LS.getLightDetected();
+
+        while(LightValue < .5) {
+            LightValue = LS.getLightDetected();
+            motorRight.setPower(1);
+            motorLeft.setPower(1);
+        }
+            //90 degrees left turn
+            motorRight.setPower(1);
+            motorLeft.setPower(0);
+            sleep(500);
+
+            //Forward 2.25 sqrs.
+            motorRight.setPower(1);
+            motorLeft.setPower(1);
+            sleep(964);
+    }
+}
