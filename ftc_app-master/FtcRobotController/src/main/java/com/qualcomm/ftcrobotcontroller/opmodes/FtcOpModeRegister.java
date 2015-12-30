3ac68ab92266b3c341c3c1de5155115517ce1fbe
lambor gini <lambor gini>
2015-12-30 15:57:44 -0800

    Started Mountain for blue

--- ftc_app-master/FtcRobotController/src/main/java/com/qualcomm/ftcrobotcontroller/opmodes/FtcOpModeRegister.java
+++ ftc_app-master/FtcRobotController/src/main/java/com/qualcomm/ftcrobotcontroller/opmodes/FtcOpModeRegister.java
@@ -55,7 +55,10 @@ public class FtcOpModeRegister implements OpModeRegister {
     manager.register("K9TeleOp", Gyro_One.class);
     manager.register ("PushBotAuto", PushBotAuto.class);
     manager.register("TestAuto",TestAuto.class);
-    manager.register("HopefullAuto",BeaconBlue.class);
+    manager.register("BeBlue",BeaconBlue.class);
+    manager.register("BeRed",BeaconRed.class);
+    manager.register("YamaBlue",MountianBlue.class);
+
     //manager.register ("Beacon", Autonomous.class);
 
     /*
